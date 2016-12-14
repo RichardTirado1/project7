1f6774b6bdf873a287bebdbb26966199b1d42182
Richard Tirado <richardtirado1@yahoo.com>
2016-12-14 08:07:48 -0800

    help

--- project7.js
+++ project7.js
@@ -13,12 +13,12 @@ function transformPoint(event) {
 // Step 2: drawSquare and drawCircle functions
 function drawSquare(x, y, size, color) {
   var newsquare = document.createElementNS(namespace, "rect")
-  newsquare.setAttribute("x", xpos)
-  newsquare.setAttribute("y", ypos)
-  newsquare.setAttribute("width", width)
-  newsquare.setAttribute("height", height)
+  newsquare.setAttribute("x", x)
+  newsquare.setAttribute("y", y)
+  newsquare.setAttribute("width", size)
+  newsquare.setAttribute("height", size)
   newsquare.setAttribute("fill", color)
-  canvas.appendChild(newsquare)
+  screen.appendChild(newsquare)
 }
 
 function drawCircle(xpos, ypos, radius, color) {
@@ -27,7 +27,7 @@ function drawCircle(xpos, ypos, radius, color) {
   newcircle.setAttribute("cy", ypos)
   newcircle.setAttribute("r", radius)
   newcircle.setAttribute("fill", color)
-  canvas.appendChild(newcircle)
+  screen.appendChild(newcircle)
 }
 
 // Step 3: Event listeners
@@ -35,7 +35,18 @@ document.addEventListener("mousemove", function(e) {
  var pt = transformPoint(e)
  var xpos = pt.x
  var ypos = pt.y
-})
+ var selectedShape = document.getElementById("shapeSelect").value
+ var selectedColor = document.getElementById("colorSelect").value
+ var selectedSize = document.getElementById("sizeSelect").value
+
+ if (selectedShape == "square") {
+      drawSquare(pt.x, pt.y, selectedSize, selectedColor)
+    }
+  else if (selectedShape == "circle") {
+       drawCircle(pt.x, pt.y, selectedSize, selectedColor)
+  }
+
+ })
 
 document.addEventListener("mousedown", function(e) {
 
